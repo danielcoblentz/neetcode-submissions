1065475c70d00e66fa49ab6725f64fd9ac68c8df
Daniel <danielcoblentz916@gmail.com>
2026-07-11 14:27:46 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,28 @@
+'''
+input
+want:
+
+edge case(s)
+
+time, space
+
+exp:
+we use the bitwise not operator to check eac hvlue and the all function which return true if there are all truthy vlaues within it
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = [c.lower() for c in s if c.isalnum()]
+        return all(s[i] == s[~i] for i in range(len(s) // 2))
