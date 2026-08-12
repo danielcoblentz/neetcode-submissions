26e597a04332106f8ef0a8eada8725cee046934a
Daniel <danielcoblentz916@gmail.com>
2026-08-12 12:07:38 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool dfs(int node, unordered_map<int, vector<int>>& adj, vector<int>& states){
+        if (states[node] == 1) return false;
+        if (states[node] == 2) return true;
+
+        states[node] = 1;
+        for (auto& nei : adj[node]){
+            if (!dfs(nei, adj, states)) return false;
+        }
+        states[node] = 2;
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>>adj;
+        int n = numCourses;
+
+        //make graph
+        for (auto& p : prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
+        vector<int>states(n, 0);
+
+        for (int i = 0; i < n; i++){
+            if (!dfs(i, adj, states)) return false;
+        }
+        return true;
+    }
+};
